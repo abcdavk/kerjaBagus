f2016993e448e593f49072a87b5fa0ee15b58c2b
Farell Dio Revianzha <farelldio1@gmail.com>
2026-07-13 23:23:52 +0700

    update:navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 z-50 flex items-center justify-between w-full mx-auto bg-background py-4 px-15 tracking-wide">
+      <div id="logo">
+        <Image
+          src="/logo/kerjabagus_icon.svg"
+          alt="Kerjabagus logo"
+          width={32}
+          height={32}
+          className="h-8 w-auto"
+        />
+      </div>
+      <ul id="nav-links" className="flex gap-7">
+        <li className="py-4 md:py-0">
+          <a href="" className="hover:font-semibold text-xl">
+            Beranda
+          </a>
+        </li>
+        <li className="py-4 md:py-0">
+          <a href="" className="hover:font-semibold text-xl">
+            Profil
+          </a>
+        </li>
+        <li className="py-4 md:py-0">
+          <a href="" className="hover:font-semibold text-xl">
+            Lowongan
+          </a>
+        </li>
+        <li className="py-4 md:py-0">
+          <a href="" className="hover:font-semibold text-xl">
+            CV Generator
+          </a>
+        </li>
+        <li className="py-4 md:py-0">
+          <a href="" className="hover:font-semibold text-xl">
+            Panduan
+          </a>
+        </li>
+      </ul>
+
+      <div className="flex gap-4">
+        <button
+          className="text-[#F4991A] border border-[#F4991A] py-2 px-9 rounded-md
+         transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103
+          cursor-pointer"
+        >
+          Masuk
+        </button>
+        <button
+          className="bg-[#F4991A] text-white  py-2 px-9 rounded-md
+         transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103
+          cursor-pointer"
+        >
+          Daftar
+        </button>
+      </div>
+    </nav>
+  );
+}
