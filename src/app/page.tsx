cf4af821be7d89d155f831176692c780d46ea51b
abcdavk <xloot.mcpedl@gmail.com>
2026-07-14 12:42:58 +0700

    update: working on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import "./page.css";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <h1>Homepage</h1>
-    </div>
+    <main className="flex flex-col flex-1 font-sans">
+      <section className="hero-section-container">
+        <div className="hero-section-content">
+          <h1>Hweo section</h1>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </section>
+      <section className="cta-container">
+        <div className="cta-content">
+          <h1>CTA</h1>
+        </div>
+      </section>
+    </main>
   );
 }
