904acb110593db0d226eec4f6ec2240080c88d9e
Farell Dio Revianzha <farelldio1@gmail.com>
2026-07-20 20:56:17 +0700

    fix:navbar-links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { RiAddLine } from "@remixicon/react";
 
 export default function Navbar() {
   return (
@@ -17,45 +18,46 @@ export default function Navbar() {
       </div>
       <ul id="nav-links" className="flex gap-7">
         <li className="py-4 md:py-0">
-          <a href="" className="hover:font-semibold text-xl">
+          <Link href="/" className="hover:font-semibold text-xl">
             Beranda
-          </a>
+          </Link>
         </li>
         <li className="py-4 md:py-0">
-          <a href="" className="hover:font-semibold text-xl">
+          <Link href="" className="hover:font-semibold text-xl">
             Profil
-          </a>
+          </Link>
         </li>
         <li className="py-4 md:py-0">
-          <a href="" className="hover:font-semibold text-xl">
+          <Link href="/jobs" className="hover:font-semibold text-xl">
             Lowongan
-          </a>
+          </Link>
         </li>
         <li className="py-4 md:py-0">
-          <a href="" className="hover:font-semibold text-xl">
+          <Link href="" className="hover:font-semibold text-xl">
             CV Generator
-          </a>
+          </Link>
         </li>
         <li className="py-4 md:py-0">
-          <a href="" className="hover:font-semibold text-xl">
+          <Link href="" className="hover:font-semibold text-xl">
             Panduan
-          </a>
+          </Link>
         </li>
       </ul>
 
       <div className="flex gap-4">
         <Link
           href="/login"
-          className="text-[#F4991A] border border-[#F4991A] py-2 px-9 rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103 cursor-pointer"
+          className="text-[#77746E] border border-[#F6D39E] bg-[#FBF6F0] py-2 px-9 rounded-lg transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103 cursor-pointer"
         >
           Masuk
         </Link>
 
         <Link
-          href="/register"
-          className="bg-[#F4991A] text-white py-2 px-9 rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103 cursor-pointer"
+          href="/"
+          className="bg-[#F4991A] text-white py-2 px-4 rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-103 cursor-pointer"
         >
-          Daftar
+          <RiAddLine size={20} className="inline mr-2" />
+          Post Pekerjaan
         </Link>
       </div>
     </nav>
